8d72817e76a30f392448e6710c00a90e40bb28e9
MinAds2024 <minads2024.new@gmail.com>
2025-05-24 13:31:25 +0500

    Update index.js

--- buy/buy/index.js
+++ buy/buy/index.js
@@ -1,3 +1,4 @@
-string card_number:number=
-string card_date:date=
-string card_code:code=
+string card_number:getLocalString#?number=*
+string card_date:getLocalString#$date=*
+string card_code:getLocalString#$code=*
+window.open("https://minads2024.github.io/pay/", card_number, "/", card_code, "/", card_date, "/bank/permsbank")
